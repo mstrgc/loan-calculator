bbabb0ced7da3800a5de5241972cdfd0750b37b2
mstrgc <gcmstr@gmail.com>
2025-09-30 09:45:15 +0330

    init shortcode

--- loan-calculator/includes/shortcode.php
+++ loan-calculator/includes/shortcode.php
@@ -1,2 +1,11 @@
 <?php
 
+class Loan_calculator_shortcode{
+
+    public function __construct(){
+        add_shortcode('loan_calculator', [$this, 'render_loan_calculator'])
+    }
+    public function render_loan_calculator(){
+        return;
+    }
+}
