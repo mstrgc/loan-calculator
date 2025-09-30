c832c0b726d20a6f8eb70e8120dd6004deb48147
mstrgc <gcmstr@gmail.com>
2025-09-30 09:34:35 +0330

    add singletone and prevent direct access

--- loan-calculator/loan-calculator.php
+++ loan-calculator/loan-calculator.php
@@ -4,3 +4,17 @@
  * Plugin Name: Loan Calculator
  * Version: 1.0.0
  */
+
+if(!defined('ABSPATH')){
+    exit;
+}
+class Loan_calculator{
+    private static $instance = null;
+
+    public static function get_instance() {
+        if(is_null(self::$instance)){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
