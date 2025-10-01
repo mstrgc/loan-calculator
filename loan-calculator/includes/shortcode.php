8e4a5000ea00b4c3b8762edab61c6df5127a3ff2
mstrgc <gcmstr@gmail.com>
2025-10-01 16:50:54 +0330

    separate shortcode template

--- loan-calculator/includes/shortcode.php
+++ loan-calculator/includes/shortcode.php
@@ -85,33 +85,7 @@ class Loan_calculator_shortcode{
 
     public function render_loan_calculator(){
         ob_start();
-        ?>
-            <div>
-                <form id="loan_form">
-                    <?php wp_nonce_field() ?>
-                    <input type="radio" name="loan_or_average" value="average" id="average" checked>
-                    <label for="average">میانگین</label>
-                    <br>
-                    <input type="radio" name="loan_or_average" value="loan" id="loan">
-                    <label for="loan">وام</label>
-                    <br>
-                    <input type="text" name="price" id="average" value="1000000">
-                    <label for="price">مبلغ</label>
-                    <br>
-                    <input type="range" name="date" id="date" min="6" max="60" step="6" value="6">
-                    <label for="date">مدت زمان</label>
-                    <br>
-                    <input type="range" name="time" id="time" min="1" max="12" value="1">
-                    <label for="time">مدت زمان واریز به حساب</label>
-                    <br>
-                    <label><input type="radio" name="fee" value="0" checked>0%</label>
-                    <label><input type="radio" name="fee" value="2">2%</label>
-                    <label><input type="radio" name="fee" value="4">4%</label>
-                    <label for="fee">کارمزد</label>
-                </form>
-                <p id="result"></p>
-            </div>
-        <?php
+        include plugin_dir_path(__FILE__) . '../templates/render-shortcode.php';
         return ob_get_clean();
     }
 }
