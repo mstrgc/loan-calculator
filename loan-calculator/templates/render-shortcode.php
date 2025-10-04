13cd15f663f6a64368a149ad9175609528b615e9
mstrgc <gcmstr@gmail.com>
2025-10-04 15:39:26 +0330

    add styles to sections

--- loan-calculator/templates/render-shortcode.php
+++ loan-calculator/templates/render-shortcode.php
@@ -1,25 +1,30 @@
 <div id="loan_calculator_container">
     <form id="loan_form">
         <?php wp_nonce_field() ?>
-        <input type="radio" name="loan_or_average" value="average" id="average" checked>
-        <label for="average">میانگین</label>
-        <br>
-        <input type="radio" name="loan_or_average" value="loan" id="loan">
-        <label for="loan">وام</label>
-        <br>
-        <input type="text" name="display_price" id="display_price" value="۱,۰۰۰,۰۰۰">
-        <label for="price">مبلغ</label>
-        <br>
-        <input type="range" name="date" id="date" min="6" max="60" step="6" value="6">
-        <label for="date">مدت زمان</label>
-        <br>
-        <input type="range" name="time" id="time" min="1" max="12" value="1">
-        <label for="time">مدت زمان واریز به حساب</label>
-        <br>
-        <label><input type="radio" name="fee" value="0" checked>۰%</label>
-        <label><input type="radio" name="fee" value="2">۲%</label>
-        <label><input type="radio" name="fee" value="4">۴%</label>
-        <label for="fee">کارمزد</label>
+        <div class="loan-calculator-section1">
+            <label for="average">میانگین</label>
+            <input type="radio" name="loan_or_average" value="average" id="average" checked>
+            <br>
+            <label for="loan">وام</label>
+            <input type="radio" name="loan_or_average" value="loan" id="loan">
+            <br>
+            <label for="price">مبلغ</label>
+            <input type="text" name="display_price" id="display_price" value="۱,۰۰۰,۰۰۰">
+        </div>
+        <div class="loan_calculator_section2">
+            <label for="date">مدت زمان</label>
+            <input type="range" name="date" id="date" min="6" max="60" step="6" value="6">
+            <br>
+            <label for="time">مدت زمان واریز به حساب</label>
+            <input type="range" name="time" id="time" min="1" max="12" value="1">
+            <br>
+            <label for="fee">کارمزد</label>
+            <label><input type="radio" name="fee" value="0" checked>۰%</label>
+            <label><input type="radio" name="fee" value="2">۲%</label>
+            <label><input type="radio" name="fee" value="4">۴%</label>
+        </div>
     </form>
+    <div class="loan_calculator_section3">
     <p id="result"></p>
+    </div>
 </div>
