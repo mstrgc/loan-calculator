a82c0a427088444eb319e852cd3c809896029382
mstrgc <gcmstr@gmail.com>
2025-10-04 11:31:51 +0330

    replace the condition to avoid missing 0 with false

--- loan-calculator/assets/js/form.js
+++ loan-calculator/assets/js/form.js
@@ -1,7 +1,7 @@
 window.loan_plugin_js = window.loan_plugin_js || {};
 
 loan_plugin_js.persian_numbers = function(input) {
-    persian_numbers = {
+    persian_number = {
         0: '۰',
         1: '۱',
         2: '۲',
@@ -18,11 +18,38 @@ loan_plugin_js.persian_numbers = function(input) {
 
     for (let i = 0; i < input.length; i++) {
         num = Array.from(String(input))[i];
-        if(persian_numbers[num]){
-            result += persian_numbers[num];
+        if(persian_number.hasOwnProperty(num)){
+            result += persian_number[num];
         } else {
             result += num;
         }
     }
     return result;
-}
+};
+
+loan_plugin_js.english_numbers = function(input) {
+    english_number = {
+        '۰': 0,
+        '۱': 1,
+        '۲': 2,
+        '۳': 3,
+        '۴': 4,
+        '۵': 5,
+        '۶': 6,
+        '۷': 7,
+        '۸': 8,
+        '۹': 9
+    };
+
+    result = '';
+
+    for (let i = 0; i < input.length; i++) {
+        num = Array.from(String(input))[i];
+        if(english_number.hasOwnProperty(num)){
+            result += english_number[num];
+        } else {
+            result += num;
+        }
+    }
+    return result;
+};
