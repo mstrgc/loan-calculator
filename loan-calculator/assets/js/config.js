3ba12eb0d3394bc838c1a40afe93f5a64628291c
mstrgc <gcmstr@gmail.com>
2025-10-06 09:07:50 +0330

    add comments to config.js

--- loan-calculator/assets/js/config.js
+++ loan-calculator/assets/js/config.js
@@ -1,16 +1,21 @@
 let price_value = document.getElementById('display_price');
 
+//display price input in persian
 price_value.addEventListener('input', function () {
     this.value = loan_plugin_js.persian_numbers(this.value);
 });
 
 function calculate() {
+    //create formdata from html form
     let loan_form = document.getElementById('loan_form');
     let loan_form_data = new FormData(loan_form);
 
+    //add php calculator function to formdata
     loan_form_data.append('action', 'calculator');
+    //convert price value to english
     loan_form_data.append('price', loan_plugin_js.english_numbers(price_value.value));
 
+    //display date, time, fee values in result section
     loan_plugin_js.label_date_time_fee();
 
     fetch(loan_config_variables.admin_ajax_url, { method: 'post', body: loan_form_data })
