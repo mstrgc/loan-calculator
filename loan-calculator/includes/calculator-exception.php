84d81f60593dbd342740c382ab2b2e77a5a716b7
mstrgc <gcmstr@gmail.com>
2025-10-15 15:05:32 +0330

    add construct method

--- loan-calculator/includes/calculator-exception.php
+++ loan-calculator/includes/calculator-exception.php
@@ -5,5 +5,10 @@ if(!defined('ABSPATH')){
 }
 
 class Calculator_exception extends Exception{
+    protected $user_error = '';
 
+    public function __construct(string $user_error, string $logMessage = '', int $code = 0, Throwable $previous = null) {
+        parent::__construct($logMessage ?: '', $code, $previous);
+        $this->user_error = $user_error;
+    }
 }
