9405b8d21267cef50ced6f9e7f2a3d8f39431cb7
mstrgc <gcmstr@gmail.com>
2025-10-22 15:08:57 +0330

    add mehr loan calculator class

---
+++ loan-calculator/includes/bank-mehr/calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+if(!defined('ABSPATH')){
+    exit;
+}
+class Mehr_loan_calculator{
+    private static $instance = null;
+
+    public static function get_instance() {
+        if(is_null(self::$instance)){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+
+add_action('plugins_loaded', ['Mehr_loan_calculator', 'get_instance']);
