3548934936ae7824f5bf780c18098352f96b412e
mstrgc <gcmstr@gmail.com>
2025-10-26 17:08:27 +0330

    validate data in display_result function

--- loan-calculator/assets/bank-mehr/js/config.js
+++ loan-calculator/assets/bank-mehr/js/config.js
@@ -10,7 +10,7 @@ function calculate() {
         let request = fetch(loan_config_variables.admin_ajax_url, {method: 'POST', body: loan_form_data});
 
         request.then(function(response){
-            if(!resp.ok){
+            if(!response.ok){
                 throw new Error('ارتباط با سرور برقرار نشد');
             }
             return response.json();
@@ -32,21 +32,26 @@ function display_result(input) {
         error_text.style = 'display: block';
         error_text.textContent = input.data['message'];
     } else{
-        tags = '';
 
-        payment_value = document.getElementById('mehr_payment').value;
-        price_value = document.getElementById('mehr_price').value;
+        if(input.data == null){
+            error_text.textContent = 'خطا در دریافت اطلاعات';
+        } else{
+            tags = '';
 
-        payment_description = 'تسهيلات به مبلغ ' + window.number_converter.persian_numbers(price_value) + ' با بازپرداخت ' + window.number_converter.persian_numbers(payment_value) + ' قسط ۱ ماهه با مبلغ تقريبي ' + window.number_converter.persian_numbers(input.data['payment']) + ' ريال';
+            payment_value = document.getElementById('mehr_payment').value;
+            price_value = document.getElementById('mehr_price').value;
 
-        for(let i = 0; i < input.data['deposit'].length; i++) {
-            tags += (
-                '<tr><td>' + window.number_converter.persian_numbers(i + 1) + '</td><td>' + window.number_converter.persian_numbers(input.data['deposit'][i]) + '</td><td>' + payment_description + '</td></tr>'
-            );
+            payment_description = 'تسهيلات به مبلغ ' + window.number_converter.persian_numbers(price_value) + ' با بازپرداخت ' + window.number_converter.persian_numbers(payment_value) + ' قسط ۱ ماهه با مبلغ تقريبي ' + window.number_converter.persian_numbers(input.data['payment']) + ' ريال';
+
+            for(let i = 0; i < input.data['deposit'].length; i++) {
+                tags += (
+                    '<tr><td>' + window.number_converter.persian_numbers(i + 1) + '</td><td>' + window.number_converter.persian_numbers(input.data['deposit'][i]) + '</td><td>' + payment_description + '</td></tr>'
+                );
+            }
+
+            document.getElementById('mehr_placeholder').style = 'display: none;';
+            tbody.innerHTML = tags;
         }
-
-        document.getElementById('mehr_placeholder').style = 'display: none;';
-        tbody.innerHTML = tags;
     }
 }
 
