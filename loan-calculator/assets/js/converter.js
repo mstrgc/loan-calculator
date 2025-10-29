12adac4b674544242b84b1046f17d575b411004b
mstrgc <gcmstr@gmail.com>
2025-10-29 13:08:11 +0330

    add error handler

--- loan-calculator/assets/js/converter.js
+++ loan-calculator/assets/js/converter.js
@@ -1,105 +1,107 @@
 (function(){
     'use strict';
 
-//text_error = document.getElementById('error');
-
-function to_persian(input) {
-    let persian_number = {
-        0: '۰',
-        1: '۱',
-        2: '۲',
-        3: '۳',
-        4: '۴',
-        5: '۵',
-        6: '۶',
-        7: '۷',
-        8: '۸',
-        9: '۹'
-    };
-
-    let result = '';
-
-    //convert numbers to persian numbers
-    let converter = function(valid_input) {
-        for (let i = 0; i < valid_input.length; i++) {
-            let num = Array.from(valid_input)[i];
-            //ignore unmatched characters and remove them
-            if(persian_number.hasOwnProperty(num)){
-                result += persian_number[num];
-            } else if(Object.values(persian_number).includes(num)) {
-                //pass numbers if already persian
-                result += num;
-            }
+    function error_handler(input, func){
+        if(typeof input =='string' || typeof input =='number'){
+            return func(input);
+        } else{
+            document.getElementById('error').textContent = 'ورودی مبلغ باید فقط شامل اعداد باشد';
         }
-    };
-
-    //add thousand separator to numbers
-    let comma_separated_result = function(valid_input) {
-        let reversed_result = valid_input.split('').reverse().join('').replaceAll(',', '');
-        let separated_result = reversed_result.match(/.{1,3}/g);
-        return separated_result.join(',').split('').reverse().join('');
-    };
-
-    if(typeof input === 'string'){
-        converter(input);
-    } else if(typeof input === 'number'){
-        converter(String(input));
-    } else {
-        return ;//this.text_error.textContent = 'ورودی مبلغ باید فقط شامل اعداد باشد';
     }
 
-    return comma_separated_result(result);
-};
+    function to_persian(input) {
+        let persian_number = {
+            0: '۰',
+            1: '۱',
+            2: '۲',
+            3: '۳',
+            4: '۴',
+            5: '۵',
+            6: '۶',
+            7: '۷',
+            8: '۸',
+            9: '۹'
+        };
 
-if(!String.prototype.to_persian){
-    Object.defineProperty(String.prototype, 'to_persian', {'value': function() {return to_persian(this)}});
-}
+        let result = '';
 
-if(!Number.prototype.to_persian){
-    Object.defineProperty(Number.prototype, 'to_persian', {'value': function() {return to_persian(this)}});
-}
+        //convert numbers to persian numbers
+        let converter = function(valid_input) {
+            for (let i = 0; i < valid_input.length; i++) {
+                let num = Array.from(valid_input)[i];
+                //ignore unmatched characters and remove them
+                if(persian_number.hasOwnProperty(num)){
+                    result += persian_number[num];
+                } else if(Object.values(persian_number).includes(num)) {
+                    //pass numbers if already persian
+                    result += num;
+                }
+            }
+        };
 
-function to_english() {
-    let english_number = {
-        '۰': 0,
-        '۱': 1,
-        '۲': 2,
-        '۳': 3,
-        '۴': 4,
-        '۵': 5,
-        '۶': 6,
-        '۷': 7,
-        '۸': 8,
-        '۹': 9
+        //add thousand separator to numbers
+        let comma_separated_result = function(valid_input) {
+            let reversed_result = valid_input.split('').reverse().join('').replaceAll(',', '');
+            let separated_result = reversed_result.match(/.{1,3}/g);
+            return separated_result.join(',').split('').reverse().join('');
+        };
+
+        if(typeof input === 'string'){
+            converter(input);
+        } else if(typeof input === 'number'){
+            converter(String(input));
+        }
+
+        return comma_separated_result(result);
     };
 
-    let result = '';
-
-    if(typeof this === 'string'){
-        //convert persian numbers to english numbers
-        for(let i = 0; i < this.length; i++){
-            let num = Array.from(this)[i];
-            //ignore unmatched characters and remove them
-            if(english_number.hasOwnProperty(num)){
-                result += english_number[num];
-            } else if(Number(num)) {
-                //pass numbers if already english
-                result += num;
-            }
-        }
-    } else {
-        return;//this.text_error.textContent = 'ورودی مبلغ باید فقط شامل اعداد باشد';
+    if(!String.prototype.to_persian){
+        Object.defineProperty(String.prototype, 'to_persian', {'value': function() {return error_handler(this, to_persian)}});
     }
 
-    return Number(result);
-};
+    if(!Number.prototype.to_persian){
+        Object.defineProperty(Number.prototype, 'to_persian', {'value': function() {return error_handler(this, to_persian)}});
+    }
 
-if(!String.prototype.to_english){
-    Object.defineProperty(String.prototype, 'to_english', {'value': function() {return to_english(this)}});
-}
+    function to_english() {
+        let english_number = {
+            '۰': 0,
+            '۱': 1,
+            '۲': 2,
+            '۳': 3,
+            '۴': 4,
+            '۵': 5,
+            '۶': 6,
+            '۷': 7,
+            '۸': 8,
+            '۹': 9
+        };
 
-if(!Number.prototype.to_english){
-    Object.defineProperty(Number.prototype, 'to_english', {'value': function() {return to_english(this)}});
-}
+        let result = '';
+
+        if(typeof this === 'string'){
+            //convert persian numbers to english numbers
+            for(let i = 0; i < this.length; i++){
+                let num = Array.from(this)[i];
+                //ignore unmatched characters and remove them
+                if(english_number.hasOwnProperty(num)){
+                    result += english_number[num];
+                } else if(Number(num)) {
+                    //pass numbers if already english
+                    result += num;
+                }
+            }
+        }
+
+        return Number(result);
+    };
+
+    if(!String.prototype.to_english){
+        Object.defineProperty(String.prototype, 'to_english', {'value': function() {return error_handler(this, to_english)}});
+    }
+
+    if(!Number.prototype.to_english){
+        Object.defineProperty(Number.prototype, 'to_english', {'value': function() {return error_handler(this, to_english)}});
+    }
 
 })();
