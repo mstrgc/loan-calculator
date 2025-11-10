239d4fc3e1da313582ea66d52c67b99a09e4ca17
mstrgc <gcmstr@gmail.com>
2025-11-10 09:59:44 +0330

    add nonce

--- loan-calculator/banks/bank-resalat/ui.php
+++ loan-calculator/banks/bank-resalat/ui.php
@@ -1,1 +1,5 @@
-<p>hello</p>
+<div id="resalat_container_wrapper">
+    <div id="resalat_container">
+        <?php wp_nonce_field('loan_calculator_nonce', 'loan_calculator_nonce_field') ?>
+    </div>
+</div>
