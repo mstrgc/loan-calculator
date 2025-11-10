a4ba9ef03de700f890976992eeb69d6a293b4e50
mstrgc <gcmstr@gmail.com>
2025-11-10 10:06:55 +0330

    add inputs

--- loan-calculator/banks/bank-resalat/ui.php
+++ loan-calculator/banks/bank-resalat/ui.php
@@ -1,5 +1,19 @@
 <div id="resalat_container_wrapper">
     <div id="resalat_container">
         <?php wp_nonce_field('loan_calculator_nonce', 'loan_calculator_nonce_field') ?>
+        <div class="resalat_calc_type">
+            <label for="calc_type">
+                <input name="calc_type" type="radio" value="price">مبلغ وام
+            </label>
+            <label for="calc_type">
+                <input name="calc_type" type="radio" value="deposit">مبلغ سپرده
+            </label>
+            <label for="calc_type">
+                <input name="calc_type" type="radio" value="deposit_duration">زمان نگهداشت سپرده
+            </label>
+            <label for="calc_type">
+                <input name="calc_type" type="radio" value="payment">تعداد اقساط
+            </label>
+        </div>
     </div>
 </div>
