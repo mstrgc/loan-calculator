a45a1a638f094dc10b6920c7468838baf812bd97
mstrgc <gcmstr@gmail.com>
2025-11-10 09:57:52 +0330

    add ui.php file

--- loan-calculator/banks/bank-resalat/calculator.php
+++ loan-calculator/banks/bank-resalat/calculator.php
@@ -15,6 +15,8 @@ class Resalat_loan_calculator{
     }
 
     public function render(){
-
+        ob_start();
+        include_once LC_PLUGIN_MAIN_PATH . 'banks/bank-resalat/ui.php';
+        return ob_get_clean();
     }
 }
